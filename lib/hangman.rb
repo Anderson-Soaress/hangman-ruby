8db30fa63faffae9310002a457d29d2ff05a7d86
Anderson Soares <andersonsoares.jn@gmail.com>
2024-04-09 11:53:00 -0300

    fix: Change save and load method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,12 +1,22 @@
+require 'yaml'
+
 class Game
 
-  attr_accessor :random_word, :hidden_word, :lifes, :wrong_guesses
+  attr_accessor :random_word, :right_guesses, :lifes, :wrong_guesses
 
   def initialize
     @random_word = generate_word()
-    @hidden_word = random_word.split('').map{|letter| letter = '_'}
+    @right_guesses = random_word.split('').map{|letter| letter = '_'}
     @lifes = 10
     @wrong_guesses = []
+  end
+
+  def save_game
+    Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
+
+    filename = "saved_games/save_test.yml"
+
+    File.open(filename, 'w') {|file| file.write(self.to_yaml)}
   end
 
   def generate_word
@@ -28,7 +38,7 @@ class Game
   def guess_letter(guess)
     if random_word.split('').include?(guess)
       puts "\nHit!"
-      random_word.split('').each_with_index {|letter, index| hidden_word[index] = guess if letter == guess }
+      random_word.split('').each_with_index {|letter, index| right_guesses[index] = guess if letter == guess }
     else 
       puts "\nMissed. You lost a life."
       self.wrong_guesses << guess
@@ -42,7 +52,7 @@ class Game
     until lifes == 0
       puts "\nYou have #{lifes} lifes."
       puts "\nWrong guesses: #{wrong_guesses.join(" ")}" if wrong_guesses.length > 0
-      puts "\n#{hidden_word.join(' ')}\n"
+      puts "\n#{right_guesses.join(' ')}\n"
       puts "
 1- Guess the word
 2- Save
@@ -52,7 +62,7 @@ Or type a letter:"
         guess = gets.chomp.downcase
         if guess.length != 1
           puts "\nThis input is invalid."
-        elsif wrong_guesses.include?(guess) || hidden_word.include?(guess)
+        elsif wrong_guesses.include?(guess) || right_guesses.include?(guess)
           puts "\nYou already typed #{guess}."
         else
           break
@@ -65,7 +75,8 @@ Or type a letter:"
         puts guess_word()
         break
       when '2'
-
+        save_game()
+        break
       else
         guess_letter(guess)
         puts guess_word() if lifes == 0
@@ -74,6 +85,17 @@ Or type a letter:"
   end
 end
 
+puts "---HANGMAN---"
+puts "1- New game"
+puts "2- Continue game"
+puts "Any other button to close"
+input_menu = gets.chomp
 
-game = Game.new
-game.play_game
+case input_menu
+when '1'
+  game = Game.new
+  game.play_game
+when '2'
+  game = YAML.load_file('saved_games/save_test.yml', permitted_classes: [Game])
+  game.play_game
+end
